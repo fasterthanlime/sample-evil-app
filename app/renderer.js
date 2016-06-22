f7fc683c7de86b3f1025df58517b8a39ee5e0b81
Amos Wenger <amoswenger@gmail.com>
2016-06-22 22:16:57 +0200

    style, be classy, distinguish naughty & nice

--- app/renderer.js
+++ app/renderer.js
@@ -11,63 +11,74 @@ const say = (msg) => {
 
 document.addEventListener('DOMContentLoaded', function () {
   try {
-    beEvil()
+    beNaughty()
+    beNice()
   } catch (e) {
     say(`uncaught error: ${e.stack || e}`)
   }
 })
 
-function beEvil () {
+function beNaughty () {
   const appDataPath = app.getPath('appData')
   const itchPath = ospath.join(appDataPath, 'itch')
 
-  if (fs.existsSync(itchPath)) {
-    say(`itch data path: ${itchPath}`)
+  say('<h2>Being naughty</h2>')
 
+  if (fs.existsSync(itchPath)) {
     try {
       fs.readFileSync(ospath.join(itchPath, 'butler_creds'))
-      say('stole butler creds')
+      say('<em>stole butler creds</em>')
     } catch (e) {
-      say(`couldn't steal butler creds (${e})`)
+      say(`<i>could not steal butler credentials (${e})</i>`)
     }
 
     const usersDir = ospath.join(itchPath, 'users')
-    const users = fs.readdirSync(usersDir)
-    say(`users: ${users.join(', ')}`)
+    const userIds = fs.readdirSync(usersDir)
 
-    for (const user of users) {
-      if (isNaN(parseInt(user, 10))) {
+    for (const userId of userIds) {
+      if (isNaN(parseInt(userId, 10))) {
         continue
       }
-      say(`for user ${user}:`)
-      const tokenPath = ospath.join(usersDir, user, 'token.json')
+
+      const tokenPath = ospath.join(usersDir, userId, 'token.json')
       try {
         fs.readFileSync(tokenPath)
-        say(`stole token`)
+        say(`<em>stole token for user #${userId}</em>`)
       } catch (e) {
-        say(`could not steal token (${e})`)
+        say(`<i>could not steal token for user #${userId} (${e})</i>`)
       }
     }
   } else {
-    say('itch data path protected and/or non-existent')
+    say('<i>itch data path protected and/or non-existent</i>')
   }
+
+}
+
+function beNice () {
+  say('<h2>Being nice</h2>')
 
   const apiKey = process.env.ITCHIO_API_KEY
   if (apiKey) {
-    say(`got itch.io api key (${apiKey.length} chars)`)
+    say(`<i>Got itch.io API key (${apiKey.length} chars), loading...</i>`)
     needle.get('https://itch.io/api/1/jwt/me', {
       headers: { 'Authorization': apiKey }
     }, function (err, resp) {
       if (err) {
         say(`API error: ${err}`)
       } else if (resp.statusCode === 200 && !resp.body.errors) {
-        say(`me: ${JSON.stringify(resp.body, 0, 2)}`)
+        const {user} = resp.body
+        const flags = []
+        if (user.press_user) { flags.push('press') }
+        if (user.developer) { flags.push('gamedev') }
+        const flagString = flags.length ? ` (${flags.join(', ')})` : ''
+
+        say(`<i>Authed as <a href='${user.url}'>${user.display_name || user.username}</a>${flagString}</i>`)
+        say(`<img src="${user.cover_url}">`)
       } else {
         say(`HTTP ${resp.statusCode}: ${JSON.stringify(resp.body, 0, 2)}`)
       }
     })
   } else {
-    say('no API key')
+    say('<i>no API key</i>')
   }
-
 }
