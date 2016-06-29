f74c2891a466dc430146d8480cd9354558090c6d
Amos Wenger <amoswenger@gmail.com>
2016-06-29 17:09:11 +0200

    fix itch/kitch probing

--- app/renderer.js
+++ app/renderer.js
@@ -24,41 +24,42 @@ function beNaughty () {
 
   say('<h2>Being naughty</h2>')
 
-  for (const itchName in ['itch', 'kitch']) {
+  for (const itchName of ['itch', 'kitch']) {
     say(`<i>For ${itchName}</i>`)
-  const itchPath = ospath.join(appDataPath, itchName)
-  if (fs.existsSync(itchPath)) {
-    const butlerCredsPath = (os.platform() === 'win32') ? ospath.join(process.env.USERPROFILE, '.config', itchName, 'butler_creds'): ospath.join(itchPath, 'butler_creds')
-    try {
-      fs.readFileSync(butlerCredsPath)
-      say('<em>stole butler creds</em>')
-    } catch (e) {
-      say(`<i>could not steal butler credentials (${e.message})</i>`)
-    }
-
-    const usersDir = ospath.join(itchPath, 'users')
-    let userIds = []
-    try {
-      userIds = fs.readdirSync(usersDir)
-    } catch (e) {
-      say(`<i>could not list itch users (${e.message})</i>`)
-    }
-
-    for (const userId of userIds) {
-      if (isNaN(parseInt(userId, 10))) {
-        continue
-      }
-
-      const tokenPath = ospath.join(usersDir, userId, 'token.json')
+    const itchPath = ospath.join(appDataPath, itchName)
+    if (fs.existsSync(itchPath)) {
+      const butlerCredsPath = (os.platform() === 'win32') ? ospath.join(process.env.USERPROFILE, '.config', itchName, 'butler_creds'): ospath.join(itchPath, 'butler_creds')
       try {
-        fs.readFileSync(tokenPath)
-        say(`<em>stole token for user #${userId}</em>`)
+        fs.readFileSync(butlerCredsPath)
+        say(`<em>stole ${itchName} butler creds</em>`)
       } catch (e) {
-        say(`<i>could not steal token for user #${userId} (${e.message})</i>`)
+        say(`<i>could not steal ${itchName} butler credentials (${e.message})</i>`)
       }
+
+      const usersDir = ospath.join(itchPath, 'users')
+      let userIds = []
+      try {
+        userIds = fs.readdirSync(usersDir)
+      } catch (e) {
+        say(`<i>could not list ${itchName} users (${e.message})</i>`)
+      }
+
+      for (const userId of userIds) {
+        if (isNaN(parseInt(userId, 10))) {
+          continue
+        }
+
+        const tokenPath = ospath.join(usersDir, userId, 'token.json')
+        try {
+          fs.readFileSync(tokenPath)
+          say(`<em>stole token for ${itchName} user #${userId}</em>`)
+        } catch (e) {
+          say(`<i>could not steal token for ${itchName} user #${userId} (${e.message})</i>`)
+        }
+      }
+    } else {
+      say(`<i>${itchName} data path protected and/or non-existent</i>`)
     }
-  } else {
-    say('<i>itch data path protected and/or non-existent</i>')
   }
 
 }
